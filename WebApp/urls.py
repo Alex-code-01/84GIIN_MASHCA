d54d1848d4d4976b4ff84fee69d1172d756df419
Alex <alexlopezmartin2001@yahoo.es>
2022-02-18 20:10:43 +0100

    configuración del servicio web

---
+++ WebApp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from WebApp import views
+
+urlpatterns = [
+    path('', views.home, name="Home"),
+    path('estaciones', views.estaciones, name="Estaciones"),
+    path('contacto', views.contacto, name="Contacto"),
+]
---
+++ WebApp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+
+def home(request):
+
+    return render(request, "WebApp/home.html")
+
+def estaciones(request):
+
+    return render(request, "WebApp/estaciones.html")
+
+def contacto(request):
+
+    return render(request, "WebApp/contacto.html")
