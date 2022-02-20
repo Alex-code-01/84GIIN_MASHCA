ee4036124349f80bca2939996e3d08c77d61191c
Alex <alexlopezmartin2001@yahoo.es>
2022-02-20 22:24:29 +0100

    Plantilla de login de usuarios

--- WebApp/urls.py
+++ WebApp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name="Home"),
     path('estaciones', views.estaciones, name="Estaciones"),
     path('contacto', views.contacto, name="Contacto"),
+    path('login', views.login, name="Login"),
 ]
--- WebApp/views.py
+++ WebApp/views.py
@@ -2,15 +2,14 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-
 def home(request):
-
     return render(request, "WebApp/home.html")
 
 def estaciones(request):
-
     return render(request, "WebApp/estaciones.html")
 
 def contacto(request):
-
     return render(request, "WebApp/contacto.html")
+
+def login(request):
+    return render(request, "WebApp/login.html")
