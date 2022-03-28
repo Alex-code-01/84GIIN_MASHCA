56cfce39e7d07786afd0050b20407c6a6e797832
Alex <alexlopezmartin2001@yahoo.es>
2022-03-28 21:32:05 +0200

    Se accede a LAT y LONG de las Estaciones

--- WebApp/views.py
+++ WebApp/views.py
@@ -1,13 +1,18 @@
 from multiprocessing import context
 from django.shortcuts import render, HttpResponse
+from Estacion.models import Estacion
 import folium
-
+from Estacion.models import Estacion
 # Create your views here.
 
 def home(request):
     return render(request, "WebApp/home.html")
 
-def estaciones(request):
+def estaciones(request):    
+    print("LAT, LONG")
+    for e in Estacion.objects.raw('SELECT * FROM Estacion_estacion'):
+        print(e.latitud, ",", e.longitud)    
+    
     m = folium.Map()
     m = m._repr_html_()
     context = {
