e3bcf4cd452d678e15b5468c7ab3c9f24e904af0
Alex <alexlopezmartin2001@yahoo.es>
2022-05-04 17:52:01 +0200

    Modularización de las funciones de las estaciones

--- Estacion/modules/estacion_funciones.py
+++ Estacion/modules/estacion_funciones.py
@@ -1,3 +1,62 @@
+import os
+import pandas as pd
+import folium
+from datetime import datetime, timedelta
+
+from Estacion.models import Estacion
+
+def represent_map(pais, zoom):
+    return folium.Map(location=pais, zoom_start=zoom)    
+
+def locate_estaciones(map, estaciones):
+    for e in estaciones:               
+        if e.formato==Estacion.FormatoChoices.DEG_MIN_SEC:
+            coordinates = [conv_DMS_a_DD(e.latitud), conv_DMS_a_DD(e.longitud)]                    
+        elif e.formato==Estacion.FormatoChoices.DEG_DEC:
+            coordinates = [float(e.latitud), float(e.longitud)]
+        html="<b>Estación:</b><br><a href=estacion/{0} target=_top class=linksEstaciones>{1}</a><br>{0}".format(e.codigo, e.nombre)         
+        folium.Marker(
+            coordinates, 
+            tooltip='Selecciona para visualizar', 
+            popup=folium.Popup(html),            
+            icon=folium.Icon(icon="cloud"),          
+        ).add_to(map)
+    return map
+
+def read_csv_file(nombre_archivo):    
+    global rows, columns, data, missing_values, my_file
+    current_dir = os.getcwd()
+    nombre_archivo = "{0}{1}{2}".format(current_dir, '\media\\', nombre_archivo)    
+    my_file = pd.read_csv(nombre_archivo)
+    data = pd.DataFrame(data=my_file, index=None)    
+    return data
+
+def select_data(data, parameter, since, until):
+    date_list, parameter_list = [], []     
+    since, until = str_to_date(since, '%Y-%m-%d'), str_to_date(until, '%Y-%m-%d')    
+    data = data[[data.columns[0], parameter]]
+    for item in range(len(data)):        
+        date = str_to_date(data[data.columns[0]][item], '%d/%m/%Y %H:%M:%S')
+        if date > since and date < until:
+            date_list.append(date_to_str(date, '%d/%m/%Y %H:%M:%S'))
+            parameter_list.append(data[parameter][item])
+    return date_list, parameter_list
+
+def convert_data(data, type):    
+    result = []
+    if type == float:        
+        result = [float(item.replace(",",".")) for item in data]
+    return result
+
+def str_to_date(date, format):
+    return datetime.strptime(date, format)
+
+def date_to_str(date, format):
+    return date.strftime(format)
+
+def date_today():
+    return datetime.today()
+
 def conv_DMS_a_DD(param):
     deg, minutes, seconds, direction = param[:2], param[2:4], param[4:6], param[6]
     result = (float(deg)+ float(minutes)/60 + float(seconds)/(60*60))*(-1 if direction in ['W', 'S'] else 1)
--- Estacion/views.py
+++ Estacion/views.py
@@ -1,39 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-import os
-import pandas as pd
-import folium
-from datetime import date, datetime
 from Estacion.models import Estacion
-from Estacion.modules import estacion_funciones as ef
+from Estacion.modules import estacion_funciones as est_func
 
 # Create your views here.
 
 @login_required
 def estaciones(request):  
-    Ecuador = [-1.4526567126141714, -78.41862277481742]  
-    m = folium.Map(location=Ecuador, zoom_start=6.5)
-    for e in Estacion.objects.raw('SELECT * FROM Estacion_estacion'):
-        #Degree, Minutes, Seconds        
-        if e.formato==Estacion.FormatoChoices.DEG_MIN_SEC:
-            lat = ef.conv_DMS_a_DD(e.latitud)
-            long = ef.conv_DMS_a_DD(e.longitud)
-        #Degree Decimal        
-        elif e.formato==Estacion.FormatoChoices.DEG_DEC:
-            lat = float(e.latitud)
-            long = float(e.longitud)    
-        html="<b>Estación:</b><br><a href=estacion/{0} target=_top class=linksEstaciones>{1}</a><br>{0}".format(e.codigo, e.nombre)         
-        folium.Marker(
-            [lat, long], 
-            tooltip='Selecciona para visualizar', 
-            popup=folium.Popup(html),            
-            icon=folium.Icon(icon="cloud"),          
-        ).add_to(m)
-
-    m = m._repr_html_()
+    Ecuador = [-1.4526567126141714, -78.41862277481742]      
+    estaciones = Estacion.objects.raw('SELECT * FROM Estacion_estacion')
+    map = est_func.represent_map(Ecuador, 6.5)    
+    map = est_func.locate_estaciones(map, estaciones)    
+    map = map._repr_html_()
     context = {
-        'm': m,
+        'map': map,
     }
     return render(request, "estaciones/estaciones.html", context)
 
@@ -49,18 +30,15 @@ def historico(request, codigo):
     date_list,  values_list, parameters_list  = [], [], []     
     parameter, since, until = None, None, None   
     if estacion.archivo_csv:                                          
-        data = read_csv_file(estacion.archivo_csv) #datos del CSV
-        parameters_list = list(data.columns[1:]) #lista de los parametros        
+        data = est_func.read_csv_file(estacion.archivo_csv)
+        parameters_list = list(data.columns[1:])
         
-        since = request.GET.get('desde', date_to_str(datetime.today(), '%Y-%m-%d'))        
-        until = request.GET.get('hasta', date_to_str(datetime.today(), '%Y-%m-%d'))                                 
-        parameter = request.GET.get('select', parameters_list[0]) #se obtiene el resultado del objeto select (por defecto se selecciona el primer parametro)
+        since = request.GET.get('desde', est_func.date_to_str(est_func.date_today(), '%Y-%m-%d'))        
+        until = request.GET.get('hasta', est_func.date_to_str(est_func.date_today(), '%Y-%m-%d'))                                 
+        parameter = request.GET.get('param', parameters_list[0])
         
-        data_selected = select_data(data, parameter, since, until)
-
-        date_list = data_selected[0]        
-        values_list = convert_data(data_selected[1], float)        
-
+        data_selected = est_func.select_data(data, parameter, since, until)
+        date_list, values_list = data_selected[0], est_func.convert_data(data_selected[1], float)        
     else: 
         print("No hay datos históricos de la estación")
     context={
@@ -80,37 +58,4 @@ def prediccion(request, codigo):
         "estacion": estacion
     } 
     return render(request, "estaciones/prediccion.html", context)
-
-def read_csv_file(nombre_archivo):    
-    global rows, columns, data, missing_values, my_file
-    current_dir = os.getcwd()
-    nombre_archivo = "{0}{1}{2}".format(current_dir, '\media\\', nombre_archivo)    
-    my_file = pd.read_csv(nombre_archivo)
-    data = pd.DataFrame(data=my_file, index=None)    
-    return data
-
-def select_data(data, parameter, since, until):
-    date_list, parameter_list = [], []     
-    since = str_to_date(since, '%Y-%m-%d')
-    until = str_to_date(until, '%Y-%m-%d')    
-    data = data[[data.columns[0], parameter]]
-    for item in range(len(data)):        
-        date = str_to_date(data[data.columns[0]][item], '%d/%m/%Y %H:%M:%S')
-        if date > since and date < until:
-            date_list.append(date_to_str(date, '%d/%m/%Y %H:%M:%S'))
-            parameter_list.append(data[parameter][item])
-
-    return date_list, parameter_list
-
-def convert_data(data, type):    
-    result = []
-    if type == float:        
-        for item in data:            
-            result.append(float(item.replace(",", ".")))
-    return result
-
-def str_to_date(date, format):
-    return datetime.strptime(date, format)
-
-def date_to_str(date, format):
-    return date.strftime(format)
+    
