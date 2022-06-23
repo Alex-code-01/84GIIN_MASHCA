a020a519a761127b6cce0c495ba7dc7503ba45ad
Alex <alexlopezmartin2001@yahoo.es>
2022-06-23 18:40:38 +0200

    Documentación de código

--- apps/Estacion/modules/modules.py
+++ apps/Estacion/modules/modules.py
@@ -5,11 +5,25 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from apps.WebApp.models import ConfigDate
 from apps.Estacion.models import Estacion
+from apps.WebApp.modules.config_modules import queryConfig
+from django.core.exceptions import ObjectDoesNotExist
 
 def represent_map(pais, zoom):
+    """
+    Returns a map located in the country and with the zoom specified
+    Atributes:
+        pais -> String specifying the country in wich the map is located initially
+        zoom -> Float value that specifies the zoom in or zoom out
+    """
     return folium.Map(location=pais, zoom_start=zoom)    
 
 def locate_estaciones(map, estaciones):
+    """
+    Returns a map locating all the estations specified
+    Atributes:
+        map -> Map represented in the HTML file
+        estaciones -> QuerySet of the stations stored in the DataBase
+    """
     for e in estaciones:               
         if e.formato==Estacion.FormatoChoices.DEG_MIN_SEC:
             coordinates = [conv_DMS_a_DD(e.latitud), conv_DMS_a_DD(e.longitud)]                    
@@ -24,13 +38,26 @@ def locate_estaciones(map, estaciones):
         ).add_to(map)
     return map
 
-def read_csv_file(nombre_archivo):    
+def read_csv_file(nombre_archivo):  
+    """
+    Returns a DataFrame of the CSV specified
+    Atributes:
+        nombre_archivo -> Name of the CSV file containing the data
+    """  
     global my_file
     archivo = os.path.join(os.getcwd(), "media", str(nombre_archivo))
     my_file = pd.read_csv(archivo)    
     return pd.DataFrame(data=my_file, index=None)
 
 def select_data(data, parameter, since, until):
+    """
+    Returns two lists containing the timeline (date_list) and the data (parameter_list) between the dates specified
+    Atributes:
+        data -> data to select from
+        parameter -> parameter to select from the data (Example: "Temperature")
+        since -> Date object that specifies the value from which the data is selected
+        until -> Date object that specifies the value up to which data is selected
+    """
     date_list, parameter_list = [], []     
     since, until = str_to_date(since, '%Y-%m-%d'), str_to_date(until, '%Y-%m-%d')    
     data = data[[data.columns[0], parameter]]
@@ -41,22 +68,50 @@ def select_data(data, parameter, since, until):
             parameter_list.append(data[parameter][item])
     return date_list, parameter_list
 
-def convert_data(data, type):    
+def convert_data(data, type): 
+    """
+    Returns the data converted to the required type
+    Atributes:
+        data -> List object containing the data
+        type -> Type to convert the data to (Example: float)
+    """   
     result = []
     if type == float:        
         result = [float(item.replace(",",".")) for item in data]
     return result
 
 def str_to_date(date, format):
+    """
+    Returns Date object of a String object
+    Atributes:
+        date -> String object to convert
+        format -> Format to convert the string to Date (Example: '%Y-%m-%d')
+    """
     return datetime.strptime(date, format)
 
 def date_to_str(date, format):
+    """
+    Returns String object of a Date object
+    Atributes:
+        date -> Date object to convert
+        format -> Format to convert the date to the string (Example: '%Y-%m-%d')
+    """
     return date.strftime(format)
 
 def date_today():
+    """
+    Returns the current date
+    """
     return datetime.today()
 
 def add_time(from_date, value, unit):
+    """
+    Returns the result of adding a certain date another time value
+    Atributes:
+        from_date -> starting date on which to operate
+        value -> number of units to add to from_date
+        unit -> unit of time (days, weeks, months, years)
+    """
     if unit == ConfigDate.UnitChoices.dias.lower():
         date = from_date + relativedelta(days=value)
     elif unit == ConfigDate.UnitChoices.semanas.lower():
@@ -69,6 +124,26 @@ def add_time(from_date, value, unit):
         date = None
     return date
 
+def get_date(parameter, operation):
+    """
+    Returns the date result of applying the limitation of the parameter    
+    Atributes:
+        parameter -> specified instance of configuration table
+        operation -> "add" for adding or "rest" for subtracting the date limit
+    """
+    try:
+        time = queryConfig(parameter)
+        if operation == "rest":
+            time[0] = -time[0]
+        return date_to_str(add_time(date_today(), time[0], time[1]), '%Y-%m-%d')       
+    except ObjectDoesNotExist:
+        return None
+
 def conv_DMS_a_DD(param):
+    """
+    Returns a Degrees Decimal (DD) value from an Degrees Minutes and Seconds (DMS) value 
+    Atributes:
+        param -> Degrees Minutes and Seconds value to convert
+    """
     deg, minutes, seconds, direction = param[:2], param[2:4], param[4:6], param[6]
     return (float(deg)+ float(minutes)/60 + float(seconds)/(60*60))*(-1 if direction in ['W', 'S'] else 1)    
--- apps/Estacion/views.py
+++ apps/Estacion/views.py
@@ -1,15 +1,9 @@
-import os
-from django.conf import settings
 from django.db import IntegrityError
 from django.contrib import messages
-from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.core.exceptions import ObjectDoesNotExist
 from .models import Estacion, EstacionSuscripcion
 from .modules import modules as mod
-from apps.WebApp.modules.config_modules import queryConfig
-from .prediction_models import Read_data as rd, one_station_model_window_model as pred_model
 
 # Create your views here.
 
@@ -32,17 +26,12 @@ def estacion(request, codigo):
     for sus in suscriptions:
         if sus.usuario == user and sus.estacion == estacion:
             suscribed = True
-            suscribe_butt = "Desuscribir"
+            suscribe_butt = "Cancelar suscripción"
     try: 
         if request.method == 'POST':            
             if not suscribed:                        
                 suscription = EstacionSuscripcion(usuario=user, estacion=estacion)                                       
-                suscription.save()      
-                #enviar confirmación
-                subject = f"Suscripción a la estación {estacion.nombre}" 
-                message = f"Hola, {user.username},\nSe acaba de suscribir a la estación {estacion.nombre}"
-                from_email = settings.EMAIL_HOST_USER            
-                #send_mail(subject, message, from_email, [request.user.email])
+                suscription.save()                  
                 messages.success(request, f"Se ha suscrito a la estación")
                 return redirect('Estacion', estacion.codigo)
             else:
@@ -61,52 +50,43 @@ def estacion(request, codigo):
 
 def historico(request, codigo):   
     estacion = Estacion.objects.get(codigo=codigo)  
-    date_list,  values_list, parameters_list  = [], [], []     
+    date_list,  values_list, parameters_list  = [], [], []
     parameter, since, until, min_date, max_date = None, None, None, None, None
 
     if request.user.has_perm('view_Administrador') or request.user.has_perm('view_EnteGubernamental'):        
         min_date=None
     else:
-        try:
-            min_time = queryConfig("historico_min_date")        
-            min_date = mod.date_to_str(mod.add_time(mod.date_today(), -min_time[0], min_time[1]), '%Y-%m-%d')                
-        except ObjectDoesNotExist:
-            min_date = None
-    try:
-        max_time = queryConfig("historico_max_date")                 
-        max_date = mod.date_to_str(mod.add_time(mod.date_today(), max_time[0], max_time[1]), '%Y-%m-%d')    
-    except ObjectDoesNotExist:
-        max_date = None
+        min_date = mod.get_date("historico_min_date", "rest")
+
+    max_date = mod.get_date("historico_max_date", "add")
+    
+    date_value = mod.date_to_str(mod.date_today(), '%Y-%m-%d')
 
     if estacion.archivo_csv:                                          
         data = mod.read_csv_file(estacion.archivo_csv)
         parameters_list = list(data.columns[1:])
-                
-        since = request.GET.get('since', mod.date_to_str(mod.date_today(), '%Y-%m-%d'))        
-        until = request.GET.get('until', mod.date_to_str(mod.date_today(), '%Y-%m-%d'))                                 
-        parameter = request.GET.get('param', parameters_list[0])
-        
-        data_selected = mod.select_data(data, parameter, since, until)
-        date_list, values_list = data_selected[0], mod.convert_data(data_selected[1], float)        
-    else: 
-        print("No hay datos históricos de la estación")
+        if request.method == 'GET':                
+            since = request.GET.get('since', date_value)        
+            until = request.GET.get('until', date_value)                                 
+            parameter = request.GET.get('param', parameters_list[0])
+            
+            data_selected = mod.select_data(data, parameter, since, until)
+            date_list, values_list = data_selected[0], mod.convert_data(data_selected[1], float)                            
     context={
-        "estacion": estacion,
-        "date_list": date_list,
-        "values_list": values_list,
-        "parameters_list": parameters_list,
-        "selected": parameter,
+        "estacion": estacion,        
+        "parameters_list": parameters_list,        
         "since": since,
         "until": until,
         "min_date": min_date,
         "max_date": max_date,
+        'date_list': date_list,
+        'values_list': values_list,
+        'selected': parameter,
     } 
     return render(request, "estaciones/historico.html", context)
 
 def prediccion(request, codigo):         
-    estacion = Estacion.objects.get(codigo=codigo)
-    archivo = os.path.join(os.getcwd(), "media", str(estacion.archivo_csv))
-    pred_model.main(archivo)
+    estacion = Estacion.objects.get(codigo=codigo)        
     context={
         "estacion": estacion,       
     } 
